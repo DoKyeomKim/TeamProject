26f953b87d438f439cf5431617e1bf90a7a922f7
shnmj <shnmje@gmail.com>
2024-04-04 16:53:22 +0900

    개인) 이력서 등록&수정, 기업) 채용공고 등록 완료

---
+++ src/main/java/com/prj/recruitscontroller/RecruitController.java
@@ -0,0 +1,18 @@
+package com.prj.recruitscontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Recruit")
+public class RecruitController {
+
+	// 공고 목록 조회 /Recruit/List
+	//@RequestMapping("/List")
+	//public String list(Model model) {
+		//
+	}
+	
+	
+
+
